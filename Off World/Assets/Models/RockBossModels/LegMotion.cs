88cb1e2e7217918d296d21d61c0f419944e02d90
Thatcher M <thatchermcclure@gmail.com>
2025-05-19 17:03:10 -0500

    ProcedurallyMovingLegs

--- Off World/Assets/Models/RockBossModels/LegMotion.cs
+++ Off World/Assets/Models/RockBossModels/LegMotion.cs
@@ -11,6 +11,8 @@ public class LegMotion : MonoBehaviour
     // How long a step takes to complete
     [SerializeField] float moveDuration;
 
+    [SerializeField] float stepOvershootFraction;
+
     // Is the leg moving?
     public bool Moving;
 
@@ -26,45 +28,57 @@ public class LegMotion : MonoBehaviour
         if (distFromHome > wantStepAtDistance)
         {
             // Start the step coroutine
-            StartCoroutine(MoveToHome());
+            StartCoroutine(Move());
         }
     }
 
-
-    IEnumerator MoveToHome()
+    IEnumerator Move()
     {
-        // Indicate we're moving (used later)
         Moving = true;
 
-        // Store the initial conditions
-        Quaternion startRot = transform.rotation;
         Vector3 startPoint = transform.position;
+        Quaternion startRot = transform.rotation;
 
         Quaternion endRot = homeTransform.rotation;
-        Vector3 endPoint = homeTransform.position;
 
-        // Time since step started
+        // Directional vector from the foot to the home position
+        Vector3 towardHome = (homeTransform.position - transform.position);
+        // Total distnace to overshoot by   
+        float overshootDistance = wantStepAtDistance * stepOvershootFraction;
+        Vector3 overshootVector = towardHome * overshootDistance;
+        // Since we don't ground the point in this simplified implementation,
+        // we restrict the overshoot vector to be level with the ground
+        // by projecting it on the world XZ plane.
+        overshootVector = Vector3.ProjectOnPlane(overshootVector, Vector3.up);
+
+        // Apply the overshoot
+        Vector3 endPoint = homeTransform.position + overshootVector;
+
+        // We want to pass through the center point
+        Vector3 centerPoint = (startPoint + endPoint) / 2;
+        // But also lift off, so we move it up by half the step distance (arbitrarily)
+        centerPoint += homeTransform.up * Vector3.Distance(startPoint, endPoint) / 2f;
+
         float timeElapsed = 0;
-
-        // Here we use a do-while loop so the normalized time goes past 1.0 on the last iteration,
-        // placing us at the end position before ending.
         do
         {
-            // Add time since last frame to the time elapsed
             timeElapsed += Time.deltaTime;
-
             float normalizedTime = timeElapsed / moveDuration;
 
-            // Interpolate position and rotation
-            transform.position = Vector3.Lerp(startPoint, endPoint, normalizedTime);
+            // Quadratic bezier curve
+            transform.position =
+              Vector3.Lerp(
+                Vector3.Lerp(startPoint, centerPoint, normalizedTime),
+                Vector3.Lerp(centerPoint, endPoint, normalizedTime),
+                normalizedTime
+              );
+
             transform.rotation = Quaternion.Slerp(startRot, endRot, normalizedTime);
 
-            // Wait for one frame
             yield return null;
         }
         while (timeElapsed < moveDuration);
 
-        // Done moving
         Moving = false;
     }
 }
